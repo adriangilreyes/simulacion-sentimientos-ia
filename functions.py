8a822aaaaf6e6bf50abc855d719fdef65ec5deec
Adrián Gil Reyes <gilreeyes.5@gmail.com>
2026-02-27 17:39:11 +0100

    Interface gráfica con barra de progreso

---
+++ functions.py
@@ -0,0 +1,27 @@
+from textblob import TextBlob
+import tkinter as tk
+
+def analizar(entrada_texto,cuadro_ia,barra_humor,porcentaje_label): 
+   frase =  entrada_texto.get()
+   blob = TextBlob(frase)
+   valor = blob.sentiment.polarity
+   cuadro_ia.delete("1.0", tk.END)
+   porcentaje = (valor + 1) * 50
+   barra_humor["value"] = porcentaje 
+   porcentaje_label.config(text=f"{porcentaje}%")
+
+   if valor > 0.5:
+       cuadro_ia.insert(tk.END,"IA is Euphoric")
+       cuadro_ia.config(bg="orange")
+   elif valor > 0:
+       cuadro_ia.insert(tk.END,"IA is Peaceful/Happy")
+       cuadro_ia.config(bg="green") 
+
+   elif valor < 0:
+    cuadro_ia.insert(tk.END,"IA is Bothers or Sad")   
+    cuadro_ia.config(bg="blue")
+
+def salir():
+    exit()
+
+ 
--- main.py
+++ main.py
@@ -1,7 +1,33 @@
 from textblob import TextBlob
+from tkinter import ttk
+import tkinter as tk
+import functions
 
-phrase = "Hoy me siento cansado"
+ventana = tk.Tk()
+ventana.title('My window with Sentiment')
+ventana.geometry("400x300")
+ventana.config(bg="#2b2b2b")      
 
-blob = TextBlob(phrase)
 
-print(blob.sentiment)
+label_instruction = tk.Label(ventana, text="How do you feel:")
+label_instruction.pack(pady=10)
+
+entrada_texto = tk.Entry(ventana,width=60)
+entrada_texto.pack(pady=5)
+
+porcentaje_label = tk.Label(ventana,text="0%", bg="#2b2b2b", fg="white",font=("Arial",12,"bold"))
+barra_humor = ttk.Progressbar(ventana,orient="horizontal",length=200,mode="determinate")
+barra_humor.pack(pady=10)
+porcentaje_label.pack(pady=10)
+
+boton_analizar = tk.Button(ventana, text="Analizar Humor", command=lambda:functions.analizar(entrada_texto,cuadro_ia,barra_humor,porcentaje_label), bg='lightblue', fg='black')
+boton_analizar.pack(pady=20)
+
+boton_salir = tk.Button(ventana, text="Salir",command=functions.salir,bg ="cyan", fg="brown")
+boton_salir.pack(pady= 25)
+
+
+cuadro_ia = tk.Text(ventana, height=4, width=40)
+cuadro_ia.pack()
+
+ventana.mainloop()
